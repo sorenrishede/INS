42ef3b2d938a7786d29842863b79aa5df8156375
sorenrishede <s.rishede@hotmail.com>
2017-03-27 13:24:10 +0200

    INS-files

---
+++ underside.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Underside</title>
+</head>
+
+<body>
+
+<header>
+<ul class="nav">
+	<li> <a href=index.php>Forside</a></li>
+	<li class="current"> <a href=underside.php>Underside</a></li>
+	</ul>
+</header>
+
+</body>
+</html>
